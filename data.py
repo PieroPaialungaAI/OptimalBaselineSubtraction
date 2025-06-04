61d1e44bb0ac72e66ea223e04d695592a3a44d3b
Piero Paialunga <piero.paialunga@hotmail.com>
2025-06-04 17:23:48 -0400

    Trying nbconvert

--- data.py
+++ data.py
@@ -2,24 +2,39 @@ import numpy as np
 import pandas as pd 
 from constants import * 
 from utils import * 
+import warnings
 
 class TimeSeriesData():
     def __init__(self, data_folder = DEFAULT_DATA_PATH):
+        warnings.simplefilter("ignore")  # Ignore all warnings
         self.city_attribute_data = pd.read_csv(data_folder+'/city_attributes.csv')
         self.temperature_data = pd.read_csv(data_folder +'/temperature.csv')
         self.preprocess_temperature_data()
+        self.build_temperature_dict_data()
+
 
 
     def select_city(self, city):
-        self.selected_city_data = self.temperature_data[[city,'datetime']]
+
+        self.selected_city_data = self.temperature_data[['datetime',city]].copy()
         self.selected_city_data = self.selected_city_data.reset_index().drop('index',axis=1)
-        self.selected_city_data['datetime'] = pd.to_datetime(self.selected_city_data['datetime'])
-        self.selected_city_data  = standardize_data(self.selected_city_data)
+        self.selected_city_data  = standardize_data(self.selected_city_data,city)
         return self.selected_city_data
 
     
     def preprocess_temperature_data(self):
         self.temperature_data = self.temperature_data.dropna().reset_index().drop('index',axis=1)
+        self.temperature_data['datetime'] = pd.to_datetime(self.temperature_data['datetime'])
+
+    
+    def build_temperature_dict_data(self):
+        city_list = set(self.temperature_data.columns.tolist())
+        city_list.remove('datetime')
+        self.temperature_dict_data = {}
+        for city in city_list:
+            self.select_city(city)
+            self.temperature_dict_data[city] = self.selected_city_data
+
 
 
 
--- utils.py
+++ utils.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd 
 from constants import * 
 
-def standardize_data(df):
+def standardize_data(df,city):
+    df = df.copy()
     df.set_index('datetime', inplace=True)
     start_date = df.index.min().normalize()
     end_date = df.index.max().normalize() + pd.Timedelta(days=1)
     full_range = pd.date_range(start=start_date, end=end_date - pd.Timedelta(hours=1), freq='H')
     df_standardized = df.reindex(full_range)
     df_standardized.index.name = 'datetime'
-    df_standardized['New York'] = df_standardized['New York'].interpolate()  # or use fillna()
+    df_standardized[city] = df_standardized[city].interpolate()  # or use fillna()
     df_standardized = df_standardized.fillna(method = 'bfill').reset_index()
     return df_standardized
